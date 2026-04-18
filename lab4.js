fe8609d80cbed22380178aaa2cc287d38f0582a4
PedroFe-ISTEC <pedro.fe@my.istec.pt>
2026-04-18 12:52:11 +0100

    lab commit

--- lab4.js
+++ lab4.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send(`
     <h1>Laboratório 4 (Web App - Node.js)</h1>
-    <p>Antes das alterações no GitHub</p>
+    <p>Depois das alterações no GitHub</p>
+    <p>Texto com as novas alterações e deployment feitos para Azure</p>
     <p>Obrigado, Pedro Fé - ISTEC LEI A Pós-Laboral</p>
   `);
 });
